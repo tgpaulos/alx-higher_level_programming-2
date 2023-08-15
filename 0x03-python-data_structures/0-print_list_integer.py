4f287a3649d4aa36b3e1df47dd0a9de0ad4fd714
tgpaulos <warsidotigist11@gmailcom>
2023-08-15 00:56:22 +0000

    correction on py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
 def print_list_integer(my_list=[]):
- integer_list = [item for item in my_list if isinstance(item,int)]
- for integer in integer_list:
-  print("{:d}".format(integer))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
