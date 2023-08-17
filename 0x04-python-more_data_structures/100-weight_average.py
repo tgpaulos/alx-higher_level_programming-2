e57467a7c10f1f12b248c45630589ce923bd1ce3
tgpaulos <warsidotigist11@gmail>
2023-08-16 17:38:00 -0700

    fixing codestyle

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,12 +3,9 @@
 def weight_average(my_list=[]):
     if not my_list:  # Check if the list is empty
         return 0
-    
     total_score = 0
     total_weight = 0
-    
     for score, weight in my_list:
         total_score += score * weight
         total_weight += weight
-        
     return total_score / total_weight
