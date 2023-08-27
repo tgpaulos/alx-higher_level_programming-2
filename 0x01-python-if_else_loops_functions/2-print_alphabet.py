3100b12b2acad120862791d1181b5779b0cf73d5
tgpaulos <warsidotigist11@gmail.com>
2023-08-27 12:33:19 -0700

    fix errors

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-"""Print the alphabet in lowercase, not followed by a new line.i"""
+"Print the alphabet in lowercase, not followed by a new line.i"
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
