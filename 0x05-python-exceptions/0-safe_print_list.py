480ef3acd322d6dbe72346c801ca30d2f1eb7a0c
tgpaulos <warsidotigist11@gmail.com>
2023-08-28 12:30:43 -0700

    fixing errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    
     ret = 0
-      
     i = 0
-    
     while i < x:
-        
         try:
             print("{}".format(my_list[i]), end="")
             ret += 1
